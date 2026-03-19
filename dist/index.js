0e98e6e8f619b90f4e159b029c4d844f089667e0
Victor Cotton <cottonvicto0@gmail.com>
2026-03-19 11:55:52 +0100

    cors vercel

--- dist/index.js
+++ dist/index.js
@@ -13,7 +13,10 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
 app.use((0, cors_1.default)({
-    origin: 'http://localhost:5173',
+    origin: [
+        'http://localhost:5173',
+        'https://loyalty-dashboard-theta.vercel.app'
+    ],
     credentials: true
 }));
 app.use(express_1.default.json());
--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,10 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://loyalty-dashboard-theta.vercel.app'
+  ],
   credentials: true
 }))
 
